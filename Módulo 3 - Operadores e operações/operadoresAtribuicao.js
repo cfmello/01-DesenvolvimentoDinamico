9930a029c5deb35cc5e7579c97288264b341852d
Cristhiano Mello <maguinho@gmail.com>
2025-03-10 19:39:34 -0300

    exercicios feitos

--- Módulo 3 - Operadores e operações/operadoresAtribuicao.js
+++ Módulo 3 - Operadores e operações/operadoresAtribuicao.js
@@ -28,3 +28,19 @@ console.log(`Após a %= 4, valor de a: ${a}`);
 // Operador de Atribuição de Exponenciação (**=)
 a **= 3;
 console.log(`Após a **= 3, valor de a: ${a}`);
+
+//Exercicio 1
+console.log('Exercício 1');
+let x = 8;
+let y = 3;
+let resultado = (x + y * 2) > (x % y) && x != y;
+console.log(resultado);
+
+//Exercicio 3
+console.log('Exercício 3');
+console.log(10 !== '10')
+
+//Exercicio 6
+console.log('Exercício 6');
+let z = 10 / 0;
+console.log(z);
